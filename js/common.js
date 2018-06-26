010f7448b497e386f1db85b03b9fad8935f0525d
DimaDidyk <didyk.dima@list.ru>
2018-06-26 03:41:18 +0300

    change clock

--- js/common.js
+++ js/common.js
@@ -8,14 +8,11 @@ $(document).ready(function(){
 
     // change color header
 	$(window).scroll(function(){
-		console.log( $(window).scrollTop() );
-
 		if( $(window).scrollTop() > 300 ){
 			$('header').addClass('scrollTop');
 		}else{
 			$('header').removeClass('scrollTop');
 		}
-
 	});
 	$(window).scroll();
 
@@ -27,22 +24,46 @@ $(document).ready(function(){
 
 
 
-    // Grab the current date
-	var currentDate = new Date(2018, 05, 26 );
-	console.log( currentDate );
+	$("#clock-hrc").countdown("2018/06/30", function(event) {
+        $(this).html(
+            // event.strftime('%D%H%M%S')
+            event.strftime('<span class="s1">0</span><span>%D</span><span>%H</span><span>%M</span><span>%S</span>')
+        );
+    });
+
+    $("#clock-cny").countdown("2018/11/30", function(event) {
+        $(this).html(
+            event.strftime('<span class="s1">0</span><span>%D</span><span>%H</span><span>%M</span><span>%S</span>')
+        );
+    });
+
+ //    // Grab the current date
+	// var currentDate = new Date(2018, 05, 29 );
+	// console.log( currentDate );
 
 
-	// Instantiate a coutdown FlipClock
-	clock1 = $('#clock-ch').FlipClock(currentDate, {
-		clockFace: 'DailyCounter',
-		countdown: true,
-		showSeconds: true
+	// // Instantiate a coutdown FlipClock
+	// clock1 = $('#clock-ch').FlipClock(currentDate, {
+	// 	clockFace: 'DailyCounter',
+	// 	countdown: true,
+	// 	showSeconds: true
+	// });
+
+	// // Instantiate a coutdown FlipClock
+	// clock2 = $('#clock-en').FlipClock(currentDate, {
+	// 	clockFace: 'DailyCounter',
+	// 	countdown: true,
+	// 	showSeconds: true
+	// });
+
+	$('.company img').mouseover(function(){
+		let src = $(this).attr('src');
+		src = src.replace('.png', '-hover.png');
+		$(this).attr('src', src);
 	});
-
-	// Instantiate a coutdown FlipClock
-	clock2 = $('#clock-en').FlipClock(currentDate, {
-		clockFace: 'DailyCounter',
-		countdown: true,
-		showSeconds: true
+	$('.company img').mouseout(function(){
+		let src = $(this).attr('src');
+		src = src.replace('-hover.png', '.png');
+		$(this).attr('src', src);
 	});
 });
