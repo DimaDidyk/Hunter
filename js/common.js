fdcb5c3aecf2c4c6af2f48d1a857ff402dd03ecc
DimaDidyk <didyk.dima@list.ru>
2018-06-29 01:20:01 +0300

    fixed, scroll animation

--- js/common.js
+++ js/common.js
@@ -61,4 +61,48 @@ $(document).ready(function(){
 		src = src.replace('-hover.png', '.png');
 		$(this).attr('src', src);
 	});
+
+
+});
+
+var anchors = [];
+var currentAnchor = -1;
+var isAnimating  = false;
+
+$(function(){
+    
+    function updateAnchors() {
+        anchors = [];
+        $('section').each(function(i, element){
+            anchors.push( $(element).offset().top );
+        });
+    }
+    
+    $('body').on('mousewheel', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        if( isAnimating ) {
+            return false;
+        }
+        isAnimating  = true;
+        // Increase or reset current anchor
+        if( e.originalEvent.wheelDelta >= 0 ) {
+            currentAnchor--;
+        }else{
+            currentAnchor++;
+        }
+        if( currentAnchor > (anchors.length - 1) 
+           || currentAnchor < 0 ) {
+            currentAnchor = 0;
+        }
+        isAnimating  = true;
+        $('html, body').animate({
+            scrollTop: parseInt( anchors[currentAnchor] )
+        }, 500, 'swing', function(){
+            isAnimating  = false;
+        });
+    });
+
+    updateAnchors();   
+    
 });
