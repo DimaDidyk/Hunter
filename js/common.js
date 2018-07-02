e70c0cdaf82d614e049e2bf7ce2edfb9d64ae9eb
DimaDidyk <didyk.dima@list.ru>
2018-07-02 17:33:50 +0300

    fix

--- js/common.js
+++ js/common.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 
     // change color header
 	$(window).scroll(function(){
-		if( $(window).scrollTop() > 300 ){
+		if( $(window).scrollTop() > $(window).height() ){
 			$('header').addClass('scrollTop');
 		}else{
 			$('header').removeClass('scrollTop');
@@ -27,22 +27,22 @@ $(document).ready(function(){
     // var clock = $('#clock-hrc').FlipClock({
     // // ... your options here
     // });
-    // // Grab the current date
-    var currentDate = new Date();
+    // // Grab the activerent date
+    var activerentDate = new Date();
 
     // Set some date in the past. In this case, it's always been since Jan 1
-    var pastDate  = new Date(currentDate.getFullYear(), 0, 1);
+    var pastDate  = new Date(activerentDate.getFullYear(), 0, 1);
 
-    // Calculate the difference in seconds between the future and current date
-    var diff1 = currentDate.getTime() / 1000 - pastDate.getTime() / 1000;
+    // Calculate the difference in seconds between the future and activerent date
+    var diff1 = activerentDate.getTime() / 1000 - pastDate.getTime() / 1000;
 
-    var diff2 = currentDate.getTime() / 1000 - pastDate.getTime() / 1000;
+    var diff2 = activerentDate.getTime() / 1000 - pastDate.getTime() / 1000;
 
 
     // Instantiate a coutdown FlipClock
     clock = $('#clock-hrc').FlipClock(diff1, {
         clockFace: 'DailyCounter',
-        showSeconds: true
+        showSeconds: true,
     });
     clock = $('#clock-cny').FlipClock(diff2, {
         clockFace: 'DailyCounter',
@@ -62,7 +62,6 @@ $(document).ready(function(){
  //        );
  //    });
 
-
     $('.side-information .information-item').click(function(){
 		$('.side-information .information-item').removeClass('active');
 		$(this).addClass('active');
@@ -76,24 +75,25 @@ $(document).ready(function(){
 
 
 
-	$('.company img').mouseover(function(){
+	$('img.hover').mouseover(function(){
 		let src = $(this).attr('src');
 		src = src.replace('.png', '-hover.png');
 		$(this).attr('src', src);
 	});
-	$('.company img').mouseout(function(){
+	$('img.hover').mouseout(function(){
 		let src = $(this).attr('src');
 		src = src.replace('-hover.png', '.png');
 		$(this).attr('src', src);
 	});
 
 
-    $("body").on("click","a", function (event) {
-        event.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1200);
-    });
+    // $("body").on("click","a", function (event) {
+    //     event.preventDefault();
+    //     var id  = $(this).attr('href'),
+    //         top = $(id).offset().top;
+    //     $('body,html').animate({scrollTop: top}, 1200);
+    // });
+
 
     jQuery.scrollSpeed(100, 800);
 
@@ -111,58 +111,118 @@ $(document).ready(function(){
 
 
 
-// jQuery(function ($) {
-//     var $doc = $(document),
-//         ratio = $doc.height() / $(window).height(), //отношение окна к общей ширене блока, чтобы тянуть весь блок.
-//         mousepos, to;
-//     $doc.on('mousedown', '#content', dragstart);
+/*
+ * jQuery liLanding v 2.1
+ *
+ * Copyright 2013, Linnik Yura | LI MASS CODE | http://masscode.ru
+ * Free to use
+ *
+ * Last Update: 19.06.2016
+ */
+(function ($) {
+    var methods = {
+        init: function (options) {
+            var p = {
+                show: function (linkEl, landingItem) {}, 
+                hide: function (linkEl, landingItem) {},
+                topMargin: 0,
+                speedFactor: .5
+            };
+            if (options) {
+                $.extend(p, options);
+            }
+            return this.each(function () {
+                var el = $(this);
+                var elPos = el.offset().top;
+                var wHalf = $(window).height()/2
+                var scrollId = function(){};
+                
+                //assign events only links with anchors
+                $('a[href^=#]',el).on('click',function(){
+                    var linkItem = $(this);
+                    if(!linkItem.is('.active')){
+                        var linkHref = linkItem.attr('href');
+                        var linkTarget = $(linkHref);
+                        var linkTargetPos = linkTarget.offset().top;
+                        var windowPos = $(window).scrollTop();
+                        var animDuration = linkTargetPos - windowPos
+                        if(animDuration < 0){
+                            animDuration = animDuration*-1  
+                        }
+                        //scroll the page to the desired block
+                        if(linkTarget.length){
+                            $('html, body').stop(true).animate({scrollTop:(linkTargetPos-parseFloat(p.topMargin))},animDuration*p.speedFactor,function(){
+                                $(window).trigger('scroll');
+                            });
+                        }
+                    }
+                    return false;
+                })
+                //stop the animation by scrolling
+                var mousewheelevt=(/Firefox/i.test(navigator.userAgent))? "DOMMouseScroll" : "mousewheel" //FF doesn't recognize mousewheel as of FF3.x
+                if (document.attachEvent) //if IE (and Opera depending on user setting)
+                    document.attachEvent("on"+mousewheelevt, function(e){
+                        $('html, body').stop(true);     
+                    });
+                else if (document.addEventListener) //WC3 browsers
+                    document.addEventListener(mousewheelevt, function(e){
+                        //e.detail //direction
+                        $('html, body').stop(true);
+                    }, false)
+                //highlight the desired link in the menu by scrolling
+                $(window).on('scroll',function(e){
+                    clearTimeout(scrollId);
+                    var windowPos = $(window).scrollTop();
+                    if(windowPos > elPos){
+                        el.addClass('landingFix');  
+                    }else{
+                        el.removeClass('landingFix');   
+                    }
+                    scrollId = setTimeout(function(){
+                        $('.landingItem').each(function(){
+                            var landingItem = $(this);
+                            var landingItemHeight = landingItem.height();
+                            var landingItemTop = landingItem.offset().top - wHalf;
+                            var linkHref = landingItem.attr('id');
+                            var linkEl = $('a[href="#'+linkHref+'"]',el);
+                            var status;
 
-//     function dragstart(e) {
-//         e.preventDefault();
-//         mousepos = e.screenY;
-//         $doc.on('mousemove.drag', drag); //в неймспейсе drag, чтобы потом отключить безболезненно для остальных листенеров
-//         $doc.one('mouseup', dragstop);
-//     }
+                            if(windowPos > landingItemTop && windowPos < (landingItemTop + landingItemHeight)){
+                                if(!linkEl.is('.active')){
+                                    linkEl.addClass('active');
+                                    if (p.show !== undefined) {
+                                        p.show(linkEl, landingItem);
+                                    }
+                                }
+                            }else{
+                                if(linkEl.is('.active')){
+                                    linkEl.removeClass('active');
+                                    if (p.hide !== undefined) {
+                                        p.hide(linkEl, landingItem);
+                                    }
+                                }
+                            }
+                        });
+                    },100);
+                })
+                $(window).trigger('scroll');
+            });
+        }
+    };
+    $.fn.liLanding = function (method) {
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Метод ' + method + ' в jQuery.liLanding не существует');
+        }
+    };
+})(jQuery);
 
-//     function drag(e) {
-//         clearTimeout(to);
-//         var delta = (e.screenY - mousepos) * ratio;
-//         to = setTimeout(function () { // таймаут чтобы события от мыши не перекрывали друг друга, 
-//             $doc.scrollTop($doc.scrollTop() + delta);
-//             mousepos = e.screenY;
-//         }, 1);
-//     }
-
-//     function dragstop() {
-//         $doc.off('mousemove.drag'); //отключаем свой mousemove.
-//     }
-
-// });
-
-// jQuery(function ($) {
-//     var $doc = $(document),
-//         ratio = $doc.height() / $doc.height(), //отношение окна к общей ширене блока, чтобы тянуть весь блок.
-//         mousepos, to;
-//     $doc.on('mousedown', '#content', dragstart);
-
-//     function dragstart(e) {
-//         e.preventDefault();
-//         mousepos = e.screenY;
-//         $doc.on('mousemove.drag', drag);
-//         $doc.one('mouseup.drag mouseout.drag', dragstop);
-//     }
-
-//     function drag(e) {
-//         clearTimeout(to);
-//         var delta = (e.screenY - mousepos) * ratio;
-//         to = setTimeout(function () { // таймаут чтобы события от мыши не перекрывали друг друга, 
-//             $doc.scrollTop($doc.scrollTop() + delta);
-//             mousepos = e.screenY;
-//         }, 1);
-//     }
-
-//     function dragstop(e) {
-//         $doc.off('mousemove.drag mouseup.drag mouseout.drag');
-//     }
-
-// });
+//Инициализация плагина
+$(window).load(function(){
+    $('.landingNav').liLanding({
+        topMargin:0
+    });
+})
